b8be5a8d7de31cb19aeaf2f553ea4551e8027315
Mohamed Ali <54620793+Elmahi92@users.noreply.github.com>
2024-09-23 14:48:06 +0000

    renamed:    ACLED DA/app.py -> ACLED DA/streamlit_app.py

--- ACLED DA/streamlit_app.py
+++ ACLED DA/streamlit_app.py
@@ -5,15 +5,13 @@ import matplotlib.pyplot as plt
 from utils import fetch_acled_data
 from datetime import datetime
 
-
-
 # Fetch data from ACLED
 st.header("Data from ACLED")
-acled_data = fetch_acled_data()
+#acled_data = fetch_acled_data()
 
-if not acled_data.empty:
+#if not acled_data.empty:
     # Display the first few rows of the fetched data
-    st.write("Fetched Data:")
-    st.dataframe(acled_data)
-else:
-    st.write("No data available for the specified parameters.")
+#    st.write("Fetched Data:")
+#    st.dataframe(acled_data)
+#else:
+#    st.write("No data available for the specified parameters.")
